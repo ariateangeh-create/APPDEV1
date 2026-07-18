df5753541550b7657845bde0c92e5e46c9cc9d1c
ariateangeh-create <angelene.ariate.laverdad@gmail.com>
2026-07-19 01:08:45 +0800

    Fixed App.jsx

--- L03_getting-started-react/src/App.jsx
+++ L03_getting-started-react/src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
       <h2>Personal Information</h2>
 
-      <p><strong>Full Name:</strong> Your Full Name</p>
+      <p><strong>Full Name:</strong> Angelene Ariate</p>
 
-      <p><strong>Course and Year:</strong> BS Information Systems - 2nd Year</p>
+      <p><strong>Course and Year:</strong> BS Information Systems - 3rd Year</p>
 
       <h2>Fun Fact</h2>
 
@@ -22,7 +22,7 @@ function App() {
       <h2>Reflection</h2>
 
       <p>
-        Throughout this course, I have learned the Git, GitHub, and also the basics of laravel, which are essential skills for us as a BSIS Student. I also learned how to use Git commands such as add,
+        Throughout this course, I have learned the Git, slight react, and also the basics of laravel, which are essential skills for us as a BSIS Student. I also learned how to use Git commands such as add,
         commit, push, pull, merge, and branch while working on different laboratory activities. These activities helped me understand how developers collaborate and manage projects efficiently. I found React
         interesting because it allows developers to build modern and interactive web applications using reusable components. I believe i will use my learninngs to prepare me for future projects and my career in technology.
       </p>
